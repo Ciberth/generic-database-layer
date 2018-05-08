7a6a50ec55991007fc9be65ede1897bcb879576d
Ciberth <thomasclauwaert@gmail.com>
2018-05-08 22:01:58 +0200

    initial commit

---
+++ provides.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from charms.reactive import Endpoint
+from charms.reactive import set_flag, clear_flag
+from charms.reactive import when, when_not
+
+class GenericDatabase(Endpoint):
+
+    @when(endpoint.{endpoint_name}.changed)
+    def _handle_changed(self):
+        technology = self.all_joined_units.received['technology']
+        if technology:
+            flag = '{endpoint_name}.' + technology + '.requested'
+            set_flag(self.expand_name(flag))
+
+    def technology(self):
+        return self.all_joined_units.received['technology']
+
---
+++ requires.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+from charms.reactive import Endpoint
+from charms.reactive import when, when_not
+from charms.reactive import set_flag, clear_flag
+from charms.reactive import data_changed
+
+class GenericDatabaseClient(Endpoint):
+    
+    def request(self, technology):
+        to_publish['technology'] = technology
+
+    def technology(self):
+        """
+        Return the technology of the generic database.
+        """
+        return self.all_joined_units.received['technology']
+
+    def databasename(self):
+        """
+        Return the name of the provided database.
+        """
+        return self.all_joined_units.received['database']
+
+    def host(self):
+        """
+        Return the host for the provided database.
+        """
+        return self.all_joined_units.received['host']
+
+    def port(self):
+        """
+        Return the port the provided database.
+        """
+        return self.all_joined_units.received['port']
+
+    def user(self):
+        """
+        Return the username for the provided database.
+        """
+        return self.all_joined_units.received['user']
+
+    def password(self):
+        """
+        Return the password for the provided database.
+        """
+        return self.all_joined_units.received['password']
