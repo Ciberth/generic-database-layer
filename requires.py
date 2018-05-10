4409bbf32958d0771af1624ce776a6f195ce8a5f
Ciberth <thomasclauwaert@gmail.com>
2018-05-10 18:12:19 +0200

    updates

--- requires.py
+++ requires.py
@@ -6,11 +6,11 @@ from charms.reactive import set_flag, clear_flag
 from charms.reactive import data_changed
 
 class GenericDatabaseClient(Endpoint):
-    # a test
-    @when('endpoint.{endpoint_name}.changed.details')
+
+    @when('endpoint.{endpoint_name}.changed')
     def _handle_postgresql_available(self):
-        set_flag(self.expand_name('endpoint.{endpoint_name}.postgresql.available'))
-        return self.all_joined_units.received['details']
+        if self.all_joined_units.received['technology']:
+            set_flag(self.expand_name('endpoint.{endpoint_name}.postgresql.available'))
 
     def request(self, technology):
         #to_publish['technology'] = technology
@@ -23,34 +23,34 @@ class GenericDatabaseClient(Endpoint):
         """
         Return the technology of the generic database.
         """
-        return self.all_joined_units.received['technology']
+        return self.all_joined_units.received['details']['technology']
 
     def databasename(self):
         """
         Return the name of the provided database.
         """
-        return self.all_joined_units.received['dbname']
+        return self.all_joined_units.received['details']['dbname']
 
     def host(self):
         """
         Return the host for the provided database.
         """
-        return self.all_joined_units.received['host']
+        return self.all_joined_units.received['details']['host']
 
     def port(self):
         """
         Return the port the provided database.
         """
-        return self.all_joined_units.received['port']
+        return self.all_joined_units.received['details']['port']
 
     def user(self):
         """
         Return the username for the provided database.
         """
-        return self.all_joined_units.received['user']
+        return self.all_joined_units.received['details']['user']
 
     def password(self):
         """
         Return the password for the provided database.
         """
-        return self.all_joined_units.received['password']
+        return self.all_joined_units.received['details']['password']
